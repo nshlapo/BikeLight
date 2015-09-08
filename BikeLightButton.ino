5dfb2ed5b4cb0a42eaab6f07f89deb1cc0424540
shanek21 <shane.kelly@students.olin.edu>
2015-09-08 11:37:27 -0400

    Merged branches.

--- BikeLightButton.ino
+++ BikeLightButton.ino
@@ -1,69 +1,129 @@
+
  /*
   Blink
   Turns on an LED on for one second, then off for one second, repeatedly.
- 
+
   This example code is in the public domain.
  */
- 
-// Pin 13 has an LED connected on most Arduino boards.
-// give it a name:
+
+// Define pin numbers
 int ledR1 = 11;
 int ledR2 = 6;
 int ledY1 = 10;
 int ledY2 = 9;
 int button = 8;
+
 unsigned long time;
 int count = 0;
 int prevState = 0;
+int buttonState = 0;
+boolean increment = false;
+int dBounce = 100;
+int timePressed = 0;
+int prevTimePress = 0;
+int blinkTime = 300;
 
 // the setup routine runs once when you press reset:
-void setup() {                
+void setup() {
   // initialize the digital pin as an output.
   pinMode(ledR1, OUTPUT);
   pinMode(ledR2, OUTPUT);
   pinMode(ledY1, OUTPUT);
   pinMode(ledY2, OUTPUT);
   pinMode(button, INPUT);
-  Serial.begin(9600);  
+  Serial.begin(9600);
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-  int buttonState = digitalRead(button);
+  buttonState = digitalRead(button);
   time = millis();
-    Serial.println(buttonState);
-    Serial.println(count);
-    Serial.println(prevState);
-  
-  
-  if(prevState == 1 && buttonState == 0) {
+    Serial.println(String(buttonState) + "//" + String(prevState) + "//" + String(count));
+
+  if(prevState == 1 && buttonState == 0) { //check for button press and release
     count ++;
   }
-  
-  if (count%2 == 0) {
-    digitalWrite(ledR1, HIGH);   // turn the LED on (HIGH is the voltage level)
-    digitalWrite(ledR2, HIGH);
-    
-    if (time%2000 < 1000) {
-      digitalWrite(ledY1, HIGH);   // turn the LED on (HIGH is the voltage level)
-      digitalWrite(ledY2, HIGH);
-    }
-    else {
-      digitalWrite(ledY1, LOW);    // turn the LED off by making the voltage LOW
+
+  switch(count%6) {
+    case 0:                       // all off
+      digitalWrite(ledY1, LOW);
       digitalWrite(ledY2, LOW);
-    }   
-     
-  }
-  
-  else {
-    if (time%2000 < 1000) {
-      digitalWrite(ledR1, HIGH);   // turn the LED on (HIGH is the voltage level)
+      digitalWrite(ledR1, LOW);
+      digitalWrite(ledR2, LOW);
+      break;
+
+    case 1:
+      digitalWrite(ledY1, LOW);    //yellow off
+      digitalWrite(ledY2, LOW);
+
+      if (time%(2*blinkTime) < (1.5*blinkTime)) {      //red flashing
+        digitalWrite(ledR1, HIGH);
+        digitalWrite(ledR2, HIGH);
+      }
+
+      else {
+      digitalWrite(ledR1, LOW);
+      digitalWrite(ledR2, LOW);
+      }
+
+      break;
+
+    case 2:
+      digitalWrite(ledR1, HIGH);   // red on
       digitalWrite(ledR2, HIGH);
-    }
-    else {
-    digitalWrite(ledR1, LOW);    // turn the LED off by making the voltage LOW
-    digitalWrite(ledR2, LOW);
-    }
+
+      if (time%(2*blinkTime) < (1.5*blinkTime)) {      // yelow flashing
+        digitalWrite(ledY1, HIGH);
+        digitalWrite(ledY2, HIGH);
+      }
+
+      else {
+        digitalWrite(ledY1, LOW);
+        digitalWrite(ledY2, LOW);
+      }
+
+      break;
+
+    case 3:                      // all on
+      digitalWrite(ledY1, HIGH);
+      digitalWrite(ledY2, HIGH);
+      digitalWrite(ledR1, HIGH);
+      digitalWrite(ledR2, HIGH);
+      break;
+
+    case 5:
+      digitalWrite(ledY1, LOW);    //yellow off
+      digitalWrite(ledY2, LOW);
+
+      if (time%(2*blinkTime) < (1.5*blinkTime)) {      //red flashing
+        digitalWrite(ledR1, HIGH);
+        digitalWrite(ledR2, HIGH);
+      }
+
+      else {
+      digitalWrite(ledR1, LOW);
+      digitalWrite(ledR2, LOW);
+      }
+
+      break;
+
+    case 4:
+      digitalWrite(ledR1, HIGH);   // red on
+      digitalWrite(ledR2, HIGH);
+
+      if (time%(2*blinkTime) < (1.5*blinkTime)) {      // yelow flashing
+        digitalWrite(ledY1, HIGH);
+        digitalWrite(ledY2, HIGH);
+      }
+
+      else {
+        digitalWrite(ledY1, LOW);
+        digitalWrite(ledY2, LOW);
+      }
+
+      break;
   }
- prevState = digitalRead(button); 
+  prevState = buttonState; // check if button was released
 }
+
+
